0ade9f4dfdfc37c1e11411986505f709dd945d38
Michael Miller <icy.arctic.fox@gmail.com>
2018-08-19 01:38:02 -0600

    Some cleanup

--- src/spectator/dsl.cr
+++ src/spectator/dsl.cr
@@ -12,11 +12,11 @@ module Spectator
     end
 
     def describe
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#describe")
+      raise NotImplementedError.new("Spectator::DSL#describe")
     end
 
     def context
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#context")
+      raise NotImplementedError.new("Spectator::DSL#context")
     end
 
     def it(description : String, &block) : Nil
@@ -25,52 +25,52 @@ module Spectator
     end
 
     def it_behaves_like
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#it_behaves_like")
+      raise NotImplementedError.new("Spectator::DSL#it_behaves_like")
     end
 
     def subject
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#subject")
+      raise NotImplementedError.new("Spectator::DSL#subject")
     end
 
     def subject!
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#subject!")
+      raise NotImplementedError.new("Spectator::DSL#subject!")
     end
 
     def let
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#let")
+      raise NotImplementedError.new("Spectator::DSL#let")
     end
 
     def let!
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#let!")
+      raise NotImplementedError.new("Spectator::DSL#let!")
     end
 
     def before_all
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#before_all")
+      raise NotImplementedError.new("Spectator::DSL#before_all")
     end
 
     def before_each
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#before_each")
+      raise NotImplementedError.new("Spectator::DSL#before_each")
     end
 
     def after_all
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#after_all")
+      raise NotImplementedError.new("Spectator::DSL#after_all")
     end
 
     def after_each
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#after_each")
+      raise NotImplementedError.new("Spectator::DSL#after_each")
     end
 
     def around_each
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#around_each")
+      raise NotImplementedError.new("Spectator::DSL#around_each")
     end
 
     def include_examples
-      raise NotImplementedError.new("Spectator::DSL::ExampleGroupDSL#include_examples")
+      raise NotImplementedError.new("Spectator::DSL#include_examples")
     end
 
     # :nodoc:
-    protected def _spec_build : Spectator::ExampleGroup
-      Spectator::ExampleGroup.new(@examples)
+    protected def _spec_build
+      ExampleGroup.new(@examples)
     end
   end
 end
