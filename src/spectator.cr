a0fbc612448ffd495f5bdd0b4ecb4b342b41fe7d
Michael Miller <icy.arctic.fox@gmail.com>
2018-08-30 14:03:29 -0600

    Extraneous debug code

--- src/spectator.cr
+++ src/spectator.cr
@@ -4,8 +4,6 @@ require "./spectator/*"
 module Spectator
   VERSION = "0.1.0"
 
-  FOO = [] of MacroId
-
   macro describe(what, source_file = __FILE__, source_line = __LINE__, &block)
     module Spectator
       module Examples
