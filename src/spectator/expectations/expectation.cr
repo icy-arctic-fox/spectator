4c2f6157afdb19cf08c2a7c89c972651cc8ff1a4
Michael Miller <icy.arctic.fox@gmail.com>
2018-10-01 20:34:26 -0600

    Define some Expectation methods

--- src/spectator/expectations/expectation.cr
+++ src/spectator/expectations/expectation.cr
@@ -1,4 +1,11 @@
 module Spectator::Expectations
   abstract class Expectation
+    getter? negated : Bool
+
+    private def initialize(@negated)
+    end
+
+    abstract def eval : Bool
+    abstract def message : String
   end
 end
--- src/spectator/expectations/value_expectation.cr
+++ src/spectator/expectations/value_expectation.cr
@@ -1,6 +1,19 @@
 require "./expectation"
 
 module Spectator::Expectations
-  class ValueExpectation < Expectation
+  class ValueExpectation(ActualType, ExpectedType) < Expectation
+    def initialize(negated,
+                   @partial : ValueExpectationPartial(ActualType),
+                   @matcher : ValueMatcher(ExpectedType))
+      super(negated)
+    end
+
+    def eval : Bool
+      @matcher.match?(@partial) ^ negated?
+    end
+
+    def message : String
+      negated? ? @matcher.negated_message(@partial) : @matcher.message(@partial)
+    end
   end
 end
