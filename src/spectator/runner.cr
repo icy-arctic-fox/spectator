a01ef9a4b7c9588529f64fb04956a07ea8da6711
Michael Miller <icy.arctic.fox@gmail.com>
2018-10-07 02:07:25 -0600

    Up-cast to get around type error

--- src/spectator/runner.cr
+++ src/spectator/runner.cr
@@ -12,7 +12,7 @@ module Spectator
           @formatter.start_example(example)
           example.run.tap do |result|
             @formatter.end_example(result)
-          end
+          end.as(Result)
         end
       end
       @formatter.end_suite(TestResults.new(results, elapsed))
