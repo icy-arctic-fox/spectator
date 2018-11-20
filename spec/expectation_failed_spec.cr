75170421950c5a7f3531e47fe2080d5356d58d08
Michael Miller <icy.arctic.fox@gmail.com>
2018-11-20 12:55:37 -0700

    Add spec for ExpectationFailed

---
+++ spec/expectation_failed_spec.cr
@@ -0,0 +1,19 @@
+require "./spec_helper"
+
+describe Spectator::ExpectationFailed do
+  describe "#expectation" do
+    it "contains the expected value" do
+      expectation = new_unsatisfied_expectation
+      error = Spectator::ExpectationFailed.new(expectation)
+      error.expectation.should eq(expectation)
+    end
+  end
+
+  describe "#message" do
+    it "is the same as the expectation's #actual_message" do
+      expectation = new_unsatisfied_expectation
+      error = Spectator::ExpectationFailed.new(expectation)
+      error.message.should eq(expectation.actual_message)
+    end
+  end
+end
