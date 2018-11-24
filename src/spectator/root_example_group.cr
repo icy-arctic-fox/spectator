bd8df38ecd63727c21a94ce1b126ece40e4ceee3
Michael Miller <icy.arctic.fox@gmail.com>
2018-11-23 23:54:54 -0700

    Docs for RootExampleGroup

--- src/spectator/root_example_group.cr
+++ src/spectator/root_example_group.cr
@@ -1,11 +1,17 @@
 require "./example_group"
 
 module Spectator
+  # Top-most group of examples and sub-groups.
+  # The root has no parent.
   class RootExampleGroup < ExampleGroup
+    # Dummy value - this should never be used.
     def what : String
       "ROOT"
     end
 
+    # Does nothing.
+    # This prevents the root group
+    # from showing up in output.
     def to_s(io)
       # ...
     end
