6f5b03dc0fb14e32053caa1a5b0745424b2214ce
Michael Miller <icy.arctic.fox@gmail.com>
2018-12-10 16:08:25 -0700

    Add top-level namespace docs

--- src/spectator/formatters.cr
+++ src/spectator/formatters.cr
@@ -1,6 +1,7 @@
 require "./formatters/*"
 
 module Spectator
+  # Reports test results to the end-user in various formats.
   module Formatters
   end
 end
--- src/spectator/matchers.cr
+++ src/spectator/matchers.cr
@@ -1,6 +1,8 @@
 require "./matchers/*"
 
 module Spectator
+  # Evaluates conditions on values and objects
+  # to determine whether they satisify test criteria.
   module Matchers
   end
 end
