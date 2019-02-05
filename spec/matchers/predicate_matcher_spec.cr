8a09ddac0499a5239be2a5e5bbf229487071d146
Michael Miller <icy.arctic.fox@gmail.com>
2019-02-05 10:36:59 -0700

    Add spec for predicate matcher

---
+++ spec/matchers/predicate_matcher_spec.cr
@@ -0,0 +1,57 @@
+require "../spec_helper"
+
+describe Spectator::Matchers::PredicateMatcher do
+  describe "#match?" do
+    context "with a true predicate" do
+      it "is true" do
+        value = "foobar"
+        partial = Spectator::Expectations::ValueExpectationPartial.new(value)
+        matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(ascii_only: Nil)).new
+        matcher.match?(partial).should be_true
+      end
+    end
+
+    context "with a false predicate" do
+      it "is false" do
+        value = "foobar"
+        partial = Spectator::Expectations::ValueExpectationPartial.new(value)
+        matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(empty: Nil)).new
+        matcher.match?(partial).should be_false
+      end
+    end
+  end
+
+  describe "#message" do
+    it "contains the actual label" do
+      value = "foobar"
+      label = "blah"
+      partial = Spectator::Expectations::ValueExpectationPartial.new(label, value)
+      matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(ascii_only: Nil)).new
+      matcher.message(partial).should contain(label)
+    end
+
+    it "contains stringified form of predicate" do
+      value = "foobar"
+      partial = Spectator::Expectations::ValueExpectationPartial.new(value)
+      matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(ascii_only: Nil)).new
+      matcher.message(partial).should contain("ascii_only")
+    end
+  end
+
+  describe "#negated_message" do
+    it "contains the actual label" do
+      value = "foobar"
+      label = "blah"
+      partial = Spectator::Expectations::ValueExpectationPartial.new(label, value)
+      matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(ascii_only: Nil)).new
+      matcher.negated_message(partial).should contain(label)
+    end
+
+    it "contains stringified form of predicate" do
+      value = "foobar"
+      partial = Spectator::Expectations::ValueExpectationPartial.new(value)
+      matcher = Spectator::Matchers::PredicateMatcher(NamedTuple(ascii_only: Nil)).new
+      matcher.negated_message(partial).should contain("ascii_only")
+    end
+  end
+end
