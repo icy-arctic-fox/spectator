e38bcd544a5dc3f1cb23020529f9e99783466b6a
Michael Miller <icy.arctic.fox@gmail.com>
2019-03-03 19:05:20 -0700

    Update CaseMatcher to use MatchData

--- spec/matchers/case_matcher_spec.cr
+++ spec/matchers/case_matcher_spec.cr
@@ -1,153 +1,172 @@
 require "../spec_helper"
 
 describe Spectator::Matchers::CaseMatcher do
-  describe "#match?" do
+  describe "#match" do
     it "compares using #===" do
       spy = SpySUT.new
       partial = new_partial(42)
       matcher = Spectator::Matchers::CaseMatcher.new(spy)
-      matcher.match?(partial).should be_true
+      matcher.match(partial)
       spy.case_eq_call_count.should be > 0
     end
 
-    context "with identical values" do
-      it "is true" do
-        value = 42
-        partial = new_partial(value)
-        matcher = Spectator::Matchers::CaseMatcher.new(value)
-        matcher.match?(partial).should be_true
-      end
-    end
+    context "returned MatchData" do
+      describe "#matched?" do
+        context "with identical values" do
+          it "is true" do
+            value = 42
+            partial = new_partial(value)
+            matcher = Spectator::Matchers::CaseMatcher.new(value)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_true
+          end
+        end
 
-    context "with different values" do
-      it "is false" do
-        value1 = 42
-        value2 = 777
-        partial = new_partial(value1)
-        matcher = Spectator::Matchers::CaseMatcher.new(value2)
-        matcher.match?(partial).should be_false
-      end
-    end
+        context "with different values" do
+          it "is false" do
+            value1 = 42
+            value2 = 777
+            partial = new_partial(value1)
+            matcher = Spectator::Matchers::CaseMatcher.new(value2)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_false
+          end
+        end
 
-    context "with the same instance" do
-      it "is true" do
-        # Box is used because it is a reference type and doesn't override the == method.
-        ref = Box.new([] of Int32)
-        partial = new_partial(ref)
-        matcher = Spectator::Matchers::CaseMatcher.new(ref)
-        matcher.match?(partial).should be_true
-      end
-    end
+        context "with the same instance" do
+          it "is true" do
+            # Box is used because it is a reference type and doesn't override the == method.
+            ref = Box.new([] of Int32)
+            partial = new_partial(ref)
+            matcher = Spectator::Matchers::CaseMatcher.new(ref)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_true
+          end
+        end
 
-    context "with different instances" do
-      context "with same contents" do
-        it "is true" do
-          array1 = [1, 2, 3]
-          array2 = [1, 2, 3]
-          partial = new_partial(array1)
-          matcher = Spectator::Matchers::CaseMatcher.new(array2)
-          matcher.match?(partial).should be_true
+        context "with different instances" do
+          context "with same contents" do
+            it "is true" do
+              array1 = [1, 2, 3]
+              array2 = [1, 2, 3]
+              partial = new_partial(array1)
+              matcher = Spectator::Matchers::CaseMatcher.new(array2)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_true
+            end
+          end
+
+          context "with different contents" do
+            it "is false" do
+              array1 = [1, 2, 3]
+              array2 = [4, 5, 6]
+              partial = new_partial(array1)
+              matcher = Spectator::Matchers::CaseMatcher.new(array2)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_false
+            end
+          end
+
+          context "with the same type" do
+            it "is true" do
+              value1 = "foobar"
+              value2 = String
+              partial = new_partial(value1)
+              matcher = Spectator::Matchers::CaseMatcher.new(value2)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_true
+            end
+          end
+
+          context "with a different type" do
+            it "is false" do
+              value1 = "foobar"
+              value2 = Array
+              partial = new_partial(value1)
+              matcher = Spectator::Matchers::CaseMatcher.new(value2)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_false
+            end
+          end
         end
       end
 
-      context "with different contents" do
-        it "is false" do
-          array1 = [1, 2, 3]
-          array2 = [4, 5, 6]
-          partial = new_partial(array1)
-          matcher = Spectator::Matchers::CaseMatcher.new(array2)
-          matcher.match?(partial).should be_false
+      describe "#message" do
+        it "mentions ===" do
+          value = 42
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::CaseMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.message.should contain("===")
+        end
+
+        it "contains the actual label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value, label)
+          matcher = Spectator::Matchers::CaseMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
+        end
+
+        it "contains the expected label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::CaseMatcher.new(value, label)
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
+        end
+
+        context "when expected label is omitted" do
+          it "contains stringified form of expected value" do
+            value1 = 42
+            value2 = 777
+            partial = new_partial(value1)
+            matcher = Spectator::Matchers::CaseMatcher.new(value2)
+            match_data = matcher.match(partial)
+            match_data.message.should contain(value2.to_s)
+          end
         end
       end
 
-      context "with the same type" do
-        it "is true" do
-          value1 = "foobar"
-          value2 = String
-          partial = new_partial(value1)
-          matcher = Spectator::Matchers::CaseMatcher.new(value2)
-          matcher.match?(partial).should be_true
+      describe "#negated_message" do
+        it "mentions ===" do
+          value = 42
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::CaseMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain("===")
         end
-      end
 
-      context "with a different type" do
-        it "is false" do
-          value1 = "foobar"
-          value2 = Array
-          partial = new_partial(value1)
-          matcher = Spectator::Matchers::CaseMatcher.new(value2)
-          matcher.match?(partial).should be_false
+        it "contains the actual label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value, label)
+          matcher = Spectator::Matchers::CaseMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
         end
-      end
-    end
-  end
 
-  describe "#message" do
-    it "mentions ===" do
-      value = 42
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::CaseMatcher.new(value)
-      matcher.message(partial).should contain("===")
-    end
+        it "contains the expected label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::CaseMatcher.new(value, label)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
+        end
 
-    it "contains the actual label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value, label)
-      matcher = Spectator::Matchers::CaseMatcher.new(value)
-      matcher.message(partial).should contain(label)
-    end
-
-    it "contains the expected label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::CaseMatcher.new(value, label)
-      matcher.message(partial).should contain(label)
-    end
-
-    context "when expected label is omitted" do
-      it "contains stringified form of expected value" do
-        value1 = 42
-        value2 = 777
-        partial = new_partial(value1)
-        matcher = Spectator::Matchers::CaseMatcher.new(value2)
-        matcher.message(partial).should contain(value2.to_s)
-      end
-    end
-  end
-
-  describe "#negated_message" do
-    it "mentions ===" do
-      value = 42
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::CaseMatcher.new(value)
-      matcher.negated_message(partial).should contain("===")
-    end
-
-    it "contains the actual label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value, label)
-      matcher = Spectator::Matchers::CaseMatcher.new(value)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    it "contains the expected label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::CaseMatcher.new(value, label)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    context "when expected label is omitted" do
-      it "contains stringified form of expected value" do
-        value1 = 42
-        value2 = 777
-        partial = new_partial(value1)
-        matcher = Spectator::Matchers::CaseMatcher.new(value2)
-        matcher.negated_message(partial).should contain(value2.to_s)
+        context "when expected label is omitted" do
+          it "contains stringified form of expected value" do
+            value1 = 42
+            value2 = 777
+            partial = new_partial(value1)
+            matcher = Spectator::Matchers::CaseMatcher.new(value2)
+            match_data = matcher.match(partial)
+            match_data.negated_message.should contain(value2.to_s)
+          end
+        end
       end
     end
   end
--- src/spectator/matchers/case_matcher.cr
+++ src/spectator/matchers/case_matcher.cr
@@ -5,27 +5,43 @@ module Spectator::Matchers
   # The values are compared with the === operator.
   struct CaseMatcher(ExpectedType) < ValueMatcher(ExpectedType)
     # Determines whether the matcher is satisfied with the value given to it.
-    # True is returned if the match was successful, false otherwise.
-    def match?(partial)
-      expected === partial.actual
+    private def match?(actual)
+      expected === actual
     end
 
     # Determines whether the matcher is satisfied with the partial given to it.
     # `MatchData` is returned that contains information about the match.
-    def match(partial) : MatchData
-      raise NotImplementedError.new("#match")
+    def match(partial)
+      values = ExpectedActual.new(partial, self)
+      MatchData.new(match?(values.actual), values)
     end
 
-    # Describes the condition that satisfies the matcher.
-    # This is informational and displayed to the end-user.
-    def message(partial)
-      "Expected #{partial.label} to semantically equal #{label} (using ===)"
-    end
+    # Match data specific to this matcher.
+    private struct MatchData(ExpectedType, ActualType) < MatchData
+      # Creates the match data.
+      def initialize(matched, @values : ExpectedActual(ExpectedType, ActualType))
+        super(matched)
+      end
 
-    # Describes the condition that won't satsify the matcher.
-    # This is informational and displayed to the end-user.
-    def negated_message(partial)
-      "Expected #{partial.label} to not semantically equal #{label} (using ===)"
+      # Information about the match.
+      def values
+        {
+          expected: @values.expected,
+          actual:   @values.actual,
+        }
+      end
+
+      # Describes the condition that satisfies the matcher.
+      # This is informational and displayed to the end-user.
+      def message
+        "#{@values.actual_label} equals #{@values.expected_label} (using ===)"
+      end
+
+      # Describes the condition that won't satsify the matcher.
+      # This is informational and displayed to the end-user.
+      def negated_message
+        "#{@values.actual_label} does not equal #{@values.expected_label} (using ===)"
+      end
     end
   end
 end
