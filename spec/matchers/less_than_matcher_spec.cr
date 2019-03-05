d35a739e602bf449aa681792e10885d1c0af5dbc
Michael Miller <icy.arctic.fox@gmail.com>
2019-03-05 16:53:15 -0700

    Update LessThanMatcher to use MatchData

--- spec/matchers/less_than_matcher_spec.cr
+++ spec/matchers/less_than_matcher_spec.cr
@@ -1,111 +1,183 @@
 require "../spec_helper"
 
 describe Spectator::Matchers::LessThanMatcher do
-  describe "#match?" do
+  describe "#match" do
     it "compares using #<" do
       spy = SpySUT.new
       partial = new_partial(spy)
       matcher = Spectator::Matchers::LessThanMatcher.new(42)
-      matcher.match?(partial).should be_true
+      matcher.match(partial)
       spy.lt_call_count.should be > 0
     end
 
-    context "with a larger value" do
-      it "is true" do
-        actual = 42
-        expected = 777
-        partial = new_partial(actual)
-        matcher = Spectator::Matchers::LessThanMatcher.new(expected)
-        matcher.match?(partial).should be_true
+    context "returned MatchData" do
+      describe "#matched?" do
+        context "with a larger value" do
+          it "is true" do
+            actual = 42
+            expected = 777
+            partial = new_partial(actual)
+            matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_true
+          end
+        end
+
+        context "with a smaller value" do
+          it "is false" do
+            actual = 777
+            expected = 42
+            partial = new_partial(actual)
+            matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_false
+          end
+        end
+
+        context "with an equal value" do
+          it "is false" do
+            value = 42
+            partial = new_partial(value)
+            matcher = Spectator::Matchers::LessThanMatcher.new(value)
+            match_data = matcher.match(partial)
+            match_data.matched?.should be_false
+          end
+        end
       end
-    end
 
-    context "with a smaller value" do
-      it "is false" do
-        actual = 777
-        expected = 42
-        partial = new_partial(actual)
-        matcher = Spectator::Matchers::LessThanMatcher.new(expected)
-        matcher.match?(partial).should be_false
+      describe "#values" do
+        context "expected" do
+          it "is the expected value" do
+            actual = 42
+            expected = 777
+            partial = new_partial(actual)
+            matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+            match_data = matcher.match(partial)
+            match_data.values[:expected].value.should eq(expected)
+          end
+
+          it "is prefixed with <" do
+            actual = 42
+            expected = 777
+            partial = new_partial(actual)
+            matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+            match_data = matcher.match(partial)
+            match_data.values[:expected].to_s.should start_with("<")
+          end
+        end
+
+        context "actual" do
+          it "is the actual value" do
+            actual = 42
+            expected = 777
+            partial = new_partial(actual)
+            matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+            match_data = matcher.match(partial)
+            match_data.values[:actual].value.should eq(actual)
+          end
+
+          context "when #matched? is true" do
+            it "is prefixed with <" do
+              actual = 42
+              expected = 777
+              partial = new_partial(actual)
+              matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_true # Sanity check.
+              match_data.values[:actual].to_s.should start_with("<")
+            end
+          end
+
+          context "when #matched? is false" do
+            it "is prefixed with >=" do
+              actual = 777
+              expected = 42
+              partial = new_partial(actual)
+              matcher = Spectator::Matchers::LessThanMatcher.new(expected)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_false # Sanity check.
+              match_data.values[:actual].to_s.should start_with(">=")
+            end
+          end
+        end
       end
-    end
 
-    context "with an equal value" do
-      it "is false" do
-        value = 42
-        partial = new_partial(value)
-        matcher = Spectator::Matchers::LessThanMatcher.new(value)
-        matcher.match?(partial).should be_false
+      describe "#message" do
+        it "mentions <" do
+          value = 42
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.message.should contain("<")
+        end
+
+        it "contains the actual label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value, label)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
+        end
+
+        it "contains the expected label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value, label)
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
+        end
+
+        context "when expected label is omitted" do
+          it "contains stringified form of expected value" do
+            value1 = 42
+            value2 = 777
+            partial = new_partial(value1)
+            matcher = Spectator::Matchers::LessThanMatcher.new(value2)
+            match_data = matcher.match(partial)
+            match_data.message.should contain(value2.to_s)
+          end
+        end
       end
-    end
-  end
 
-  describe "#message" do
-    it "mentions <" do
-      value = 42
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value)
-      matcher.message(partial).should contain("<")
-    end
+      describe "#negated_message" do
+        it "mentions <" do
+          value = 42
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain("<")
+        end
 
-    it "contains the actual label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value, label)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value)
-      matcher.message(partial).should contain(label)
-    end
+        it "contains the actual label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value, label)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
+        end
 
-    it "contains the expected label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value, label)
-      matcher.message(partial).should contain(label)
-    end
+        it "contains the expected label" do
+          value = 42
+          label = "everything"
+          partial = new_partial(value)
+          matcher = Spectator::Matchers::LessThanMatcher.new(value, label)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
+        end
 
-    context "when expected label is omitted" do
-      it "contains stringified form of expected value" do
-        value1 = 42
-        value2 = 777
-        partial = new_partial(value1)
-        matcher = Spectator::Matchers::LessThanMatcher.new(value2)
-        matcher.message(partial).should contain(value2.to_s)
-      end
-    end
-  end
-
-  describe "#negated_message" do
-    it "mentions <" do
-      value = 42
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value)
-      matcher.negated_message(partial).should contain("<")
-    end
-
-    it "contains the actual label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value, label)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    it "contains the expected label" do
-      value = 42
-      label = "everything"
-      partial = new_partial(value)
-      matcher = Spectator::Matchers::LessThanMatcher.new(value, label)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    context "when expected label is omitted" do
-      it "contains stringified form of expected value" do
-        value1 = 42
-        value2 = 777
-        partial = new_partial(value1)
-        matcher = Spectator::Matchers::LessThanMatcher.new(value2)
-        matcher.negated_message(partial).should contain(value2.to_s)
+        context "when expected label is omitted" do
+          it "contains stringified form of expected value" do
+            value1 = 42
+            value2 = 777
+            partial = new_partial(value1)
+            matcher = Spectator::Matchers::LessThanMatcher.new(value2)
+            match_data = matcher.match(partial)
+            match_data.negated_message.should contain(value2.to_s)
+          end
+        end
       end
     end
   end
--- src/spectator/matchers/less_than_matcher.cr
+++ src/spectator/matchers/less_than_matcher.cr
@@ -5,27 +5,48 @@ module Spectator::Matchers
   # The values are compared with the < operator.
   struct LessThanMatcher(ExpectedType) < ValueMatcher(ExpectedType)
     # Determines whether the matcher is satisfied with the value given to it.
-    # True is returned if the match was successful, false otherwise.
-    def match?(partial)
-      partial.actual < expected
+    private def match?(actual)
+      actual < expected
     end
 
     # Determines whether the matcher is satisfied with the partial given to it.
     # `MatchData` is returned that contains information about the match.
-    def match(partial) : MatchData
-      raise NotImplementedError.new("#match")
+    def match(partial)
+      values = ExpectedActual.new(partial, self)
+      MatchData.new(match?(values.actual), values)
     end
 
-    # Describes the condition that satisfies the matcher.
-    # This is informational and displayed to the end-user.
-    def message(partial)
-      "Expected #{partial.label} to be less than #{label} (using <)"
-    end
+    # Match data specific to this matcher.
+    private struct MatchData(ExpectedType, ActualType) < MatchData
+      # Creates the match data.
+      def initialize(matched, @values : ExpectedActual(ExpectedType, ActualType))
+        super(matched)
+      end
 
-    # Describes the condition that won't satsify the matcher.
-    # This is informational and displayed to the end-user.
-    def negated_message(partial)
-      "Expected #{partial.label} to not be less than #{label} (using <)"
+      # Information about the match.
+      def values
+        {
+          expected: PrefixedValue.new("<", @values.expected),
+          actual:   PrefixedValue.new(actual_operator, @values.actual),
+        }
+      end
+
+      # Textual operator for the actual value.
+      private def actual_operator
+        matched? ? "<" : ">="
+      end
+
+      # Describes the condition that satisfies the matcher.
+      # This is informational and displayed to the end-user.
+      def message
+        "#{@values.actual_label} is less than #{@values.expected_label} (using <)"
+      end
+
+      # Describes the condition that won't satsify the matcher.
+      # This is informational and displayed to the end-user.
+      def negated_message
+        "#{@values.actual_label} is greater than or equal to #{@values.expected_label} (using <)"
+      end
     end
   end
 end
