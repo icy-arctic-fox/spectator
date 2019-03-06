3f1d8751fecd44aa3b069ec4a0bbc69dc047eb47
Michael Miller <icy.arctic.fox@gmail.com>
2019-03-06 02:02:55 -0700

    Update HaveKeyMatcher to use MatchData

--- spec/matchers/have_key_matcher_spec.cr
+++ spec/matchers/have_key_matcher_spec.cr
@@ -1,108 +1,165 @@
 require "../spec_helper"
 
+private struct FakeKeySet
+  def has_key?(key)
+    true
+  end
+end
+
 describe Spectator::Matchers::HaveKeyMatcher do
-  describe "#match?" do
-    context "against a Hash" do
-      context "with an existing key" do
-        it "is true" do
-          hash = Hash{"foo" => "bar"}
-          key = "foo"
-          partial = new_partial(hash)
-          matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-          matcher.match?(partial).should be_true
+  describe "#match" do
+    context "returned MatchData" do
+      describe "#matched?" do
+        context "against a Hash" do
+          context "with an existing key" do
+            it "is true" do
+              hash = Hash{"foo" => "bar"}
+              key = "foo"
+              partial = new_partial(hash)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_true
+            end
+          end
+
+          context "with a non-existent key" do
+            it "is false" do
+              hash = Hash{"foo" => "bar"}
+              key = "baz"
+              partial = new_partial(hash)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_false
+            end
+          end
+        end
+
+        context "against a NamedTuple" do
+          context "with an existing key" do
+            it "is true" do
+              tuple = {foo: "bar"}
+              key = :foo
+              partial = new_partial(tuple)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_true
+            end
+          end
+
+          context "with a non-existent key" do
+            it "is false" do
+              tuple = {foo: "bar"}
+              key = :baz
+              partial = new_partial(tuple)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.matched?.should be_false
+            end
+          end
         end
       end
 
-      context "with a non-existent key" do
-        it "is false" do
-          hash = Hash{"foo" => "bar"}
-          key = "baz"
-          partial = new_partial(hash)
-          matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-          matcher.match?(partial).should be_false
+      describe "#values" do
+        context "key" do
+          it "is the expected key" do
+            tuple = {foo: "bar"}
+            key = :baz
+            partial = new_partial(tuple)
+            matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+            match_data = matcher.match(partial)
+            match_data.values[:key].should eq(key)
+          end
+        end
+
+        context "actual" do
+          context "when #keys is available" do
+            it "is the list of keys" do
+              tuple = {foo: "bar"}
+              key = :baz
+              partial = new_partial(tuple)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.values[:actual].should eq(tuple.keys)
+            end
+          end
+
+          context "when #keys isn't available" do
+            it "is the actual value" do
+              actual = FakeKeySet.new
+              key = :baz
+              partial = new_partial(actual)
+              matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+              match_data = matcher.match(partial)
+              match_data.values[:actual].should eq(actual)
+            end
+          end
         end
       end
-    end
 
-    context "against a NamedTuple" do
-      context "with an existing key" do
-        it "is true" do
+      describe "#message" do
+        it "contains the actual label" do
           tuple = {foo: "bar"}
           key = :foo
-          partial = new_partial(tuple)
+          label = "blah"
+          partial = new_partial(tuple, label)
           matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-          matcher.match?(partial).should be_true
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
         end
-      end
 
-      context "with a non-existent key" do
-        it "is false" do
+        it "contains the expected label" do
           tuple = {foo: "bar"}
-          key = :baz
+          key = :foo
+          label = "blah"
           partial = new_partial(tuple)
-          matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-          matcher.match?(partial).should be_false
+          matcher = Spectator::Matchers::HaveKeyMatcher.new(key, label)
+          match_data = matcher.match(partial)
+          match_data.message.should contain(label)
+        end
+
+        context "when the expected label is omitted" do
+          it "contains the stringified key" do
+            tuple = {foo: "bar"}
+            key = :foo
+            partial = new_partial(tuple)
+            matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+            match_data = matcher.match(partial)
+            match_data.message.should contain(key.to_s)
+          end
         end
       end
-    end
-  end
 
-  describe "#message" do
-    it "contains the actual label" do
-      tuple = {foo: "bar"}
-      key = :foo
-      label = "blah"
-      partial = new_partial(tuple, label)
-      matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-      matcher.message(partial).should contain(label)
-    end
+      describe "#negated_message" do
+        it "contains the actual label" do
+          tuple = {foo: "bar"}
+          key = :foo
+          label = "blah"
+          partial = new_partial(tuple, label)
+          matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
+        end
 
-    it "contains the expected label" do
-      tuple = {foo: "bar"}
-      key = :foo
-      label = "blah"
-      partial = new_partial(tuple)
-      matcher = Spectator::Matchers::HaveKeyMatcher.new(key, label)
-      matcher.message(partial).should contain(label)
-    end
+        it "contains the expected label" do
+          tuple = {foo: "bar"}
+          key = :foo
+          label = "blah"
+          partial = new_partial(tuple)
+          matcher = Spectator::Matchers::HaveKeyMatcher.new(key, label)
+          match_data = matcher.match(partial)
+          match_data.negated_message.should contain(label)
+        end
 
-    context "when the expected label is omitted" do
-      it "contains the stringified key" do
-        tuple = {foo: "bar"}
-        key = :foo
-        partial = new_partial(tuple)
-        matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-        matcher.message(partial).should contain(key.to_s)
-      end
-    end
-  end
-
-  describe "#negated_message" do
-    it "contains the actual label" do
-      tuple = {foo: "bar"}
-      key = :foo
-      label = "blah"
-      partial = new_partial(tuple, label)
-      matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    it "contains the expected label" do
-      tuple = {foo: "bar"}
-      key = :foo
-      label = "blah"
-      partial = new_partial(tuple)
-      matcher = Spectator::Matchers::HaveKeyMatcher.new(key, label)
-      matcher.negated_message(partial).should contain(label)
-    end
-
-    context "when the expected label is omitted" do
-      it "contains the stringified key" do
-        tuple = {foo: "bar"}
-        key = :foo
-        partial = new_partial(tuple)
-        matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
-        matcher.negated_message(partial).should contain(key.to_s)
+        context "when the expected label is omitted" do
+          it "contains the stringified key" do
+            tuple = {foo: "bar"}
+            key = :foo
+            partial = new_partial(tuple)
+            matcher = Spectator::Matchers::HaveKeyMatcher.new(key)
+            match_data = matcher.match(partial)
+            match_data.negated_message.should contain(key.to_s)
+          end
+        end
       end
     end
   end
--- src/spectator/matchers/have_key_matcher.cr
+++ src/spectator/matchers/have_key_matcher.cr
@@ -5,27 +5,44 @@ module Spectator::Matchers
   # The set is checked with the `has_key?` method.
   struct HaveKeyMatcher(ExpectedType) < ValueMatcher(ExpectedType)
     # Determines whether the matcher is satisfied with the value given to it.
-    # True is returned if the match was successful, false otherwise.
-    def match?(partial)
-      partial.actual.has_key?(expected)
+    private def match?(actual)
+      actual.has_key?(expected)
     end
 
     # Determines whether the matcher is satisfied with the partial given to it.
     # `MatchData` is returned that contains information about the match.
-    def match(partial) : MatchData
-      raise NotImplementedError.new("#match")
+    def match(partial)
+      values = ExpectedActual.new(partial, self)
+      MatchData.new(match?(values.actual), values)
     end
 
-    # Describes the condition that satisfies the matcher.
-    # This is informational and displayed to the end-user.
-    def message(partial)
-      "Expected #{partial.label} to have key #{label}"
-    end
+    # Match data specific to this matcher.
+    private struct MatchData(ExpectedType, ActualType) < MatchData
+      # Creates the match data.
+      def initialize(matched, @values : ExpectedActual(ExpectedType, ActualType))
+        super(matched)
+      end
 
-    # Describes the condition that won't satsify the matcher.
-    # This is informational and displayed to the end-user.
-    def negated_message(partial)
-      "Expected #{partial.label} to not have key #{label}"
+      # Information about the match.
+      def values
+        actual = @values.actual
+        {
+          key:    @values.expected,
+          actual: actual.responds_to?(:keys) ? actual.keys : actual,
+        }
+      end
+
+      # Describes the condition that satisfies the matcher.
+      # This is informational and displayed to the end-user.
+      def message
+        "#{@values.actual_label} has key #{@values.expected_label}"
+      end
+
+      # Describes the condition that won't satsify the matcher.
+      # This is informational and displayed to the end-user.
+      def negated_message
+        "#{@values.actual_label} does not have key #{@values.expected_label}"
+      end
     end
   end
 end
