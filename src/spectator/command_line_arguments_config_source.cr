d22b628526eee4195157818520d0b73ea7dc578a
Michael Miller <icy.arctic.fox@gmail.com>
2019-03-22 13:15:17 -0600

    Stub remaining options from Crystal's default Spec

--- src/spectator/command_line_arguments_config_source.cr
+++ src/spectator/command_line_arguments_config_source.cr
@@ -16,6 +16,13 @@ module Spectator
         parser.on("-v", "--verbose", "Verbose output using document formatter") { builder.formatter = Formatting::DocumentFormatter.new }
         parser.on("-f", "--fail-fast", "Stop testing on first failure") { builder.fail_fast }
         parser.on("-h", "--help", "Show this help") { puts parser; exit }
+        parser.on("-e", "--example STRING", "Run examples whose full nested names include STRING") { |pattern| raise NotImplementedError.new("-e") }
+        parser.on("-l", "--line LINE", "Run examples whose line matches LINE") { |line| raise NotImplementedError.new("-l") }
+        parser.on("-p", "--profile", "Display the 10 slowest specs") { raise NotImplementedError.new("-p") }
+        parser.on("--location FILE:LINE", "Run the example at line 'LINE' in the file 'FILE', multiple allowed") { |location| raise NotImplementedError.new("--location") }
+        parser.on("--junit_output OUTPUT_DIR", "Generate JUnit XML output") { |output_dir| raise NotImplementedError.new("--juni_output") }
+        parser.on("--tap", "Generate TAP output (Test Anything Protocol)") { raise NotImplementedError.new("--tap") }
+        parser.on("--no-color", "Disable colored output") { raise NotImplementedError.new("--no-color") }
       end
     end
   end
