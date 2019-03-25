72aa72781aff9eec7a4ade68856f4699c74e7029
Michael Miller <icy.arctic.fox@gmail.com>
2019-03-25 16:15:00 -0600

    Move human time to its own type to share it

--- src/spectator/formatting/runtime.cr
+++ src/spectator/formatting/runtime.cr
@@ -18,32 +18,7 @@ module Spectator::Formatting
     # ```
     def to_s(io)
       io << "Finished in "
-      io << human_time(@runtime)
-    end
-
-    # Provides a more human-friendly formatting for a time span.
-    # This produces a string with the minimum of
-    # microseconds, milliseconds, seconds, minutes, hours, or days.
-    private def human_time(span)
-      millis = span.total_milliseconds
-      return "#{(millis * 1000).round.to_i} microseconds" if millis < 1
-
-      seconds = span.total_seconds
-      return "#{millis.round(2)} milliseconds" if seconds < 1
-      return "#{seconds.round(2)} seconds" if seconds < 60
-
-      int_seconds = seconds.to_i
-      minutes = int_seconds / 60
-      int_seconds %= 60
-      return sprintf("%i:%02i", minutes, int_seconds) if minutes < 60
-
-      hours = minutes / 60
-      minutes %= 60
-      return sprintf("%i:%02i:%02i", hours, minutes, int_seconds) if hours < 24
-
-      days = hours / 24
-      hours %= 24
-      return sprintf("%i days %i:%02i:%02i", days, hours, minutes, int_seconds)
+      io << HumanTime.new(@runtime)
     end
   end
 end
