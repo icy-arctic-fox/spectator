96c271cf33326fed5d2c749f53634e24ad5eacc1
Michael Miller <icy.arctic.fox@gmail.com>
2019-08-01 14:46:47 -0600

    No need to shout

--- src/spectator/expectations/block_actual.cr
+++ src/spectator/expectations/block_actual.cr
@@ -17,7 +17,7 @@ module Spectator::Expectations
     # Creates the actual with a generic label.
     # This is used for the "should" syntax and when the label doesn't matter.
     def initialize(@proc : -> ReturnType)
-      super("<PROC>")
+      super("<Proc>")
     end
 
     # Reports complete information about the actual value.
