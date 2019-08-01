3a7dc7299aa80673857dbd6bd182d8857e5a128f
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-31 20:11:30 -0600

    Rewrite ExpectationPartial and remove sub-types
    The partial now contains the actual and source.
    It also calls the correct match method on the matcher and constructs an 
    expectation (which needs to be updated).

--- src/spectator/matchers/matcher.cr
+++ src/spectator/matchers/matcher.cr
@@ -5,13 +5,47 @@ module Spectator::Matchers
   # A matcher looks at something produced by the SUT
   # and evaluates whether it is correct or not.
   abstract struct Matcher
-    # Textual representation of what the matcher expects.
-    # This shouldn't be used in the conditional logic,
-    # but for verbose output to help the end-user.
-    abstract def label : String
+    # Short text about the matcher's purpose.
+    # This explains what condition satisfies the matcher.
+    # The description is used when the one-liner syntax is used.
+    # ```
+    # it { is_expected.to do_something }
+    # ```
+    # The phrasing should be such that it reads "it ___."
+    abstract def description : String
 
-    # Determines whether the matcher is satisfied with the value given to it.
-    # True is returned if the match was successful, false otherwise.
-    abstract def match(partial) : MatchData
+    # Message displayed when the matcher isn't satisifed.
+    # This is only called when `#matches?` returns false.
+    abstract def failure_message : String
+
+    # Message displayed when the matcher isn't satisifed and is negated.
+    # This is only called when `#does_not_match?` returns false.
+    #
+    # A default implementation of this method is provided,
+    # which causes compilation to fail.
+    # If the matcher supports negation, it must override this method.
+    def failure_message_when_negated : String
+      {% raise "Negation with #{@type.name} is not supported."}
+    end
+
+    # Checks whether the matcher is satisifed.
+    private abstract def match?(actual) : Bool
+
+    # If the expectation is negated, then this method is called instead of `#match?`.
+    # The default implementation of this method is to invert the result of `#match?`.
+    # If the matcher requires custom handling of negated matches,
+    # then this method should be overriden.
+    # Remember to override `#failure_message_when_negated` as well.
+    private def does_not_match?(actual) : Bool
+      !matches?(actual)
+    end
+
+    def match(actual)
+      matched = match?(actual)
+    end
+
+    def negated_match(actual)
+      matched = does_not_match?(actual)
+    end
   end
 end
