3b1a5a1121903297b380af711248b3fa6506a135
Michael Miller <icy.arctic.fox@gmail.com>
2019-08-08 21:52:05 -0600

    Fix various expectation type code

--- src/spectator/expectations/expectation.cr
+++ src/spectator/expectations/expectation.cr
@@ -9,7 +9,7 @@ module Spectator::Expectations
     getter source : Source
 
     # Creates the expectation.
-    def initialize(@match_data : MatchData, @source : Source)
+    def initialize(@match_data : Matchers::MatchData, @source : Source)
     end
 
     def satisfied?
@@ -21,19 +21,19 @@ module Spectator::Expectations
     end
 
     def failure_message?
-      @match_data.as?(FailedMatchData).try(&.failure_message)
+      @match_data.as?(Matchers::FailedMatchData).try(&.failure_message)
     end
 
     def failure_message
-      @match_data.as(FailedMatchData).failure_message
+      @match_data.as(Matchers::FailedMatchData).failure_message
     end
 
     def values?
-      @match_data.as?(FailedMatchData).try(&.values)
+      @match_data.as?(Matchers::FailedMatchData).try(&.values)
     end
 
     def values
-      @match_data.as(FailedMatchData).values
+      @match_data.as(Matchers::FailedMatchData).values
     end
 
     # Creates the JSON representation of the expectation.
@@ -41,18 +41,18 @@ module Spectator::Expectations
       json.object do
         json.field("source") { @source.to_json(json) }
         json.field("satisfied", satisfied?)
-        if (failed = @match_data.as?(FailedMatchData))
+        if (failed = @match_data.as?(Matchers::FailedMatchData))
           failed_to_json(failed, json)
         end
       end
     end
 
-    private def failed_to_json(failed : FailedMatchData, json : ::JSON::Builder)
+    private def failed_to_json(failed : Matchers::FailedMatchData, json : ::JSON::Builder)
       json.field("failure", failed.failure_message)
       json.field("values") do
         json.object do
           failed.values.each do |pair|
-            json.field(pair.label, pair.value)
+            json.field(pair.first, pair.last)
           end
         end
       end
