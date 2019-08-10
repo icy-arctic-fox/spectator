c4b886cad2a0f44b838998092b60fcae3745beba
Michael Miller <icy.arctic.fox@gmail.com>
2019-08-10 10:53:00 -0600

    Add match data docs

--- src/spectator/matchers/match_data.cr
+++ src/spectator/matchers/match_data.cr
@@ -1,5 +1,7 @@
 module Spectator::Matchers
+  # Information about the outcome of a match.
   abstract struct MatchData
+    # Indicates whether the match as successful or not.
     abstract def matched? : Bool
   end
 end
--- src/spectator/matchers/successful_match_data.cr
+++ src/spectator/matchers/successful_match_data.cr
@@ -1,7 +1,9 @@
 require "./match_data"
 
 module Spectator::Matchers
+  # Information about a successful match.
   struct SuccessfulMatchData < MatchData
+    # Indicates that the match succeeded.
     def matched?
       true
     end
