b8e125e38f9436106cdbcd2e2ddedc8380b0e2b5
Michael Miller <icy.arctic.fox@gmail.com>
2019-08-31 13:12:17 -0600

    Add test wrapper

---
+++ src/spectator/test_wrapper.cr
@@ -0,0 +1,30 @@
+require "../spectator_test"
+require "./source"
+
+module Spectator
+  # Stores information about a end-user test.
+  # Used to instantiate tests and run them.
+  struct TestWrapper
+    # Location of the test in source code.
+    getter source : Source
+
+    # Description the user provided for the test.
+    getter description : String
+
+    # Creates a wrapper for the test.
+    # The *builder* creates an instance of the test.
+    # The *runner* takes the test created by *builder* and runs it.
+    def initialize(@description, @source, @builder : -> SpectatorTest, @runner : SpectatorTest ->)
+    end
+
+    # Instantiates and runs the test.
+    # This method yields twice - before and after the test.
+    # The test instance is yielded.
+    def run : Nil
+      test = @builder.call
+      yield test
+      @runner.call(test)
+      yield test
+    end
+  end
+end
