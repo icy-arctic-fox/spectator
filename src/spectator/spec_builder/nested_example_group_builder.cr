5f3c9d2f6305cf0d9252d7972be795e90b38c736
Michael Miller <icy.arctic.fox@gmail.com>
2019-09-22 11:02:53 -0600

    Distinguish group variables

--- src/spectator/spec_builder/nested_example_group_builder.cr
+++ src/spectator/spec_builder/nested_example_group_builder.cr
@@ -6,9 +6,9 @@ module Spectator::SpecBuilder
     def initialize(@what : String | Symbol)
     end
 
-    def build(group)
-      context = TestContext.new(group.context, build_hooks)
-      NestedExampleGroup.new(@what, group, context).tap do |group|
+    def build(parent_group)
+      context = TestContext.new(parent_group.context, build_hooks)
+      NestedExampleGroup.new(@what, parent_group, context).tap do |group|
         group.children = children.map do |child|
           child.build(group).as(ExampleComponent)
         end
