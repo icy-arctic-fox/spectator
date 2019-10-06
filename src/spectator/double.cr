49764e5873772b5effa6e4d6acaa16b8a4a63361
Michael Miller <icy.arctic.fox@gmail.com>
2019-10-05 22:43:40 -0600

    Use do...end for multi-line stub def

--- src/spectator/double.cr
+++ src/spectator/double.cr
@@ -1,12 +1,16 @@
 module Spectator
   abstract class Double
-    macro stub(definition)
+    macro stub(definition, &block)
       delegate {{definition.name.id}}, to: @internal
 
       private class Internal
         def {{definition.name.id}}({{definition.args.splat}})
           {% if definition.block.is_a?(Nop) %}
-            raise "Stubbed method called without being allowed"
+            {% if block.is_a?(Nop) %}
+              raise "Stubbed method called without being allowed"
+            {% else %}
+              {{block.body}}
+            {% end %}
           {% else %}
             {{definition.block.body}}
           {% end %}
