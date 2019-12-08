4c9bbbe07bdd7633e8684d6928309325345e56d8
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 15:11:23 -0700

    Formatting

--- src/spectator/mocks/type_registry.cr
+++ src/spectator/mocks/type_registry.cr
@@ -2,17 +2,17 @@ module Spectator::Mocks
   module TypeRegistry
     extend self
 
-    alias Key =  Tuple(String, Symbol)
+    alias Key = Tuple(String, Symbol)
 
     @@entries = {} of Key => Deque(MethodStub)
 
     def add(type_name : String, method_name : Symbol, source : Source, args : Arguments) : Nil
       key = {type_name, method_name}
       list = if @@entries.has_key?(key)
-        @@entries[key]
-      else
-        @@entries[key] = Deque(MethodStub).new
-      end
+               @@entries[key]
+             else
+               @@entries[key] = Deque(MethodStub).new
+             end
       list << NilMethodStub.new(method_name, source, args)
     end
 
