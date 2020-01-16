cf448576e22b20e1a80a4059dbc04ae2471633b8
Arina Shmeleva <arina.shmeleva@jetrockets.ru>
2020-01-17 00:49:59 +0300

    Add test for subject

---
+++ spec/rspec/expectations/subject_spec.cr
@@ -0,0 +1,12 @@
+require "../../spec_helper"
+class Base; end
+
+Spectator.describe "Subject" do
+  subject() { Base.new }
+
+  describe "#foo" do
+    it "bar" do
+      expect(subject).to be_a(Base)
+    end
+  end
+end
