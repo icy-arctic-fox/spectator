ea6c1542245bc4abddd52d1ef29a0183df8cd5b0
Michael Miller <icy.arctic.fox@gmail.com>
2020-09-12 15:43:28 -0600

    Change version to 0.10.0
    Prepare for next minor release.

--- src/spectator.cr
+++ src/spectator.cr
@@ -6,7 +6,7 @@ module Spectator
   extend self
 
   # Current version of the Spectator library.
-  VERSION = "0.9.22"
+  VERSION = "0.10.0"
 
   # Top-level describe method.
   # All specs in a file must be wrapped in this call.
