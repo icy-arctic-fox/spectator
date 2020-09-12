b271028c1e30f786c058e2690c2362dae3e35f58
Michael Miller <icy.arctic.fox@gmail.com>
2020-09-12 15:59:20 -0600

    Remove most includes for now

--- src/spectator/includes.cr
+++ src/spectator/includes.cr
@@ -4,52 +4,7 @@
 # Including all files with a wildcard would accidentally enable should-syntax.
 # Unfortunately, that leads to the existence of this file to include everything but that file.
 
-# First the sub-modules.
-require "./dsl"
-require "./expectations"
-require "./matchers"
-require "./formatting"
-
-# Then all of the top-level types.
-require "./spec_builder"
-require "./example_component"
-require "./example"
-require "./runnable_example"
-require "./pending_example"
-
-require "./example_conditions"
-require "./example_hooks"
-require "./example_group"
-require "./nested_example_group"
-require "./root_example_group"
-
-require "./mocks"
-
 require "./config"
 require "./config_builder"
-require "./config_source"
-require "./command_line_arguments_config_source"
-
-require "./example_filter"
-require "./source_example_filter"
-require "./line_example_filter"
-require "./name_example_filter"
-require "./null_example_filter"
-require "./composite_example_filter"
-
-require "./example_failed"
-require "./expectation_failed"
-require "./test_suite"
-require "./report"
-require "./profile"
-require "./runner"
-
-require "./result"
-require "./finished_result"
-require "./successful_result"
-require "./pending_result"
-require "./failed_result"
-require "./errored_result"
-
-require "./source"
-require "./example_iterator"
+require "./dsl"
+require "./spec_builder"
