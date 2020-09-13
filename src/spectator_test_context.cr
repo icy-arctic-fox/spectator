6752c7c25401af63fc658a787ce0a3d22ce311da
Michael Miller <icy.arctic.fox@gmail.com>
2020-09-12 18:39:21 -0600

    Move DSL-based code to subclass of SpectatorContext
    This resolves a circular dependency.

---
+++ src/spectator_test_context.cr
@@ -0,0 +1,23 @@
+require "./spectator_context"
+require "./spectator/dsl"
+
+class SpectatorTestContext < SpectatorContext
+  include ::Spectator::DSL::Groups
+
+  # Initial implicit subject for tests.
+  # This method should be overridden by example groups when an object is described.
+  private def _spectator_implicit_subject
+    nil
+  end
+
+  # Initial subject for tests.
+  # Returns the implicit subject.
+  # This method should be overridden when an explicit subject is defined by the DSL.
+  # TODO: Subject needs to be cached.
+  private def subject
+    _spectator_implicit_subject
+  end
+
+  # def initialize(@spectator_test_values : ::Spectator::TestValues)
+  # end
+end
