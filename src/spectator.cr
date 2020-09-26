a39223ff21e011641293e938d67ed006c73ca6d1
Michael Miller <icy.arctic.fox@gmail.com>
2020-09-26 11:03:11 -0600

    Bump to 0.9.25

--- src/spectator.cr
+++ src/spectator.cr
@@ -6,7 +6,7 @@ module Spectator
   extend self
 
   # Current version of the Spectator library.
-  VERSION = "0.9.24"
+  VERSION = "0.9.25"
 
   # Top-level describe method.
   # All specs in a file must be wrapped in this call.
