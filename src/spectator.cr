f1ad476ae5eb341c8c7d65c4fccbbf4a10951d4d
Michael Miller <icy.arctic.fox@gmail.com>
2020-09-26 18:16:21 -0600

    Trick to use one version string from shard.yml

--- src/spectator.cr
+++ src/spectator.cr
@@ -6,7 +6,7 @@ module Spectator
   extend self
 
   # Current version of the Spectator library.
-  VERSION = "0.10.0"
+  VERSION = {{ `shards version #{__DIR__}`.stringify.chomp }}
 
   # Top-level describe method.
   # All specs in a file must be wrapped in this call.
