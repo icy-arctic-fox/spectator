aa4c257adec0e2c4374d04bd6ed303c52e46476f
Michael Miller <icy.arctic.fox@gmail.com>
2021-01-09 14:35:58 -0700

    Change Wrapper to a nested type for Lazy

--- src/spectator/lazy.cr
+++ src/spectator/lazy.cr
@@ -1,21 +1,33 @@
-require "./wrapper"
-
 module Spectator
   # Lazily stores a value.
   struct Lazy(T)
-    @wrapper : Wrapper(T)?
+    @value : Value(T)?
 
     # Retrieves the value, if it was previously fetched.
     # On the first invocation of this method, it will yield.
     # The block should return the value to store.
     # Subsequent calls will return the same value and not yield.
     def get(&block : -> T)
-      if (wrapper = @wrapper)
-        wrapper.value
+      if (value = @value)
+        value.get
       else
         yield.tap do |value|
-          @wrapper = Wrapper.new(value)
+          @value = Value.new(value)
         end
+      end
+    end
+
+    # Wrapper for a value.
+    # This is intended to be used as a union with nil.
+    # It allows storing (caching) a nillable value.
+    private struct Value(T)
+      # Creates the wrapper.
+      def initialize(@value : T)
+      end
+
+      # Retrieves the value.
+      def get : T
+        @value
       end
     end
   end
