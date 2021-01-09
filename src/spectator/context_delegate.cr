cf422eca02f0b743578029f7171eba0458243d47
Michael Miller <icy.arctic.fox@gmail.com>
2021-01-09 10:56:33 -0700

    Add null constructor

--- src/spectator/context_delegate.cr
+++ src/spectator/context_delegate.cr
@@ -1,5 +1,6 @@
 require "./context"
 require "./context_method"
+require "./null_context"
 
 module Spectator
   # Stores a test context and a method to call within it.
@@ -8,6 +9,14 @@ module Spectator
     # The *context* is the instance of the test context.
     # The *method* is proc that downcasts *context* and calls a method on it.
     def initialize(@context : Context, @method : ContextMethod)
+    end
+
+    # Creates a delegate with a null context.
+    # The context will be ignored and the block will be executed in its original scope.
+    def self.null(&block : -> _)
+      context = NullContext.new
+      method = ContextMethod.new { block.call }
+      new(context, method)
     end
 
     # Invokes a method in the test context.
