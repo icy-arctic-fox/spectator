7d5c9edab768323c59e0a16e17a6591010c9c727
Michael Miller <icy.arctic.fox@gmail.com>
2021-02-13 11:44:51 -0700

    Use cast as workaround

--- spec/spectator/value_spec.cr
+++ spec/spectator/value_spec.cr
@@ -4,32 +4,33 @@ Spectator.describe Spectator::Value do
   subject { described_class.new(42, "Test Label") }
 
   it "stores the value" do
-    expect(&.value).to eq(42)
+    # NOTE: This cast is a workaround for [issue #55](https://gitlab.com/arctic-fox/spectator/-/issues/55)
+    value = subject.as(Spectator::Value(Int32)).value
+    expect(value).to eq(42)
   end
 
-  # TODO: Fix issue with compile-time type of `subject` being a union.
-  # describe "#to_s" do
-  #   subject { super.to_s }
-  #
-  #   it "contains the label" do
-  #     is_expected.to contain("Test Label")
-  #   end
-  #
-  #   it "contains the value" do
-  #     is_expected.to contain("42")
-  #   end
-  # end
-  #
-  # describe "#inspect" do
-  #   let(value) { described_class.new([42], "Test Label") }
-  #   subject { value.inspect }
-  #
-  #   it "contains the label" do
-  #     is_expected.to contain("Test Label")
-  #   end
-  #
-  #   it "contains the value" do
-  #     is_expected.to contain("[42]")
-  #   end
-  # end
+  describe "#to_s" do
+    subject { super.to_s }
+
+    it "contains the label" do
+      is_expected.to contain("Test Label")
+    end
+
+    it "contains the value" do
+      is_expected.to contain("42")
+    end
+  end
+
+  describe "#inspect" do
+    let(value) { described_class.new([42], "Test Label") }
+    subject { value.inspect }
+
+    it "contains the label" do
+      is_expected.to contain("Test Label")
+    end
+
+    it "contains the value" do
+      is_expected.to contain("[42]")
+    end
+  end
 end
