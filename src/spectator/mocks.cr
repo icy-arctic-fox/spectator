5f61a24656dfc1ee331415a1fb7cee5b34d4c012
Michael Miller <icy.arctic.fox@gmail.com>
2021-07-10 14:18:07 -0600

    Provide default stub for Process.exit
    Prevent terminating the process from within a test.

--- src/spectator/mocks.cr
+++ src/spectator/mocks.cr
@@ -1,4 +1,5 @@
 require "./mocks/*"
+require "./system_exit"
 
 module Spectator
   # Functionality for mocking existing types.
@@ -10,4 +11,14 @@ module Spectator
       Registry.reset
     end
   end
+end
+
+# Add default stub to `exit` method.
+# This captures *most* (technically not all) attempts to exit the process.
+# This stub only takes effect in example code.
+# It intercepts `exit` calls and raises `Spectator::SystemExit` to prevent killing the test.
+class ::Process
+  include ::Spectator::Mocks::Stubs
+
+  stub self.exit(code) { raise ::Spectator::SystemExit.new }
 end
