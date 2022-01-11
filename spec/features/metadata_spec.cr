c1841526d4f21fb9005b8176ffe82366c3fe0832
Michael Miller <icy.arctic.fox@gmail.com>
2022-01-10 21:49:54 -0700

    Test string interpolation with sample

--- spec/features/metadata_spec.cr
+++ spec/features/metadata_spec.cr
@@ -6,4 +6,14 @@ Spectator.describe "Spec metadata" do
   it "supports #{interpolation}" do |example|
     expect(example.name).to eq("supports string interpolation")
   end
+
+  def self.various_strings
+    %w[foo bar baz]
+  end
+
+  sample various_strings do |string|
+    it "works with #{string}" do |example|
+      expect(example.name).to eq("works with #{string}")
+    end
+  end
 end
