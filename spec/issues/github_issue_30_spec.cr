a8e55e32d2fefe0215f97333d3f072583082f09b
Michael Miller <icy.arctic.fox@gmail.com>
2022-03-30 19:13:14 -0600

    Works now due to lazy doubles

--- spec/issues/github_issue_30_spec.cr
+++ spec/issues/github_issue_30_spec.cr
@@ -1,9 +1,9 @@
 require "../spec_helper"
 
 Spectator.describe "GitHub Issue #30" do
-  # let(dbl) { double(:foo) }
+  let(dbl) { double(:foo) }
 
-  xit "supports block-less symbol doubles", pending: "Mock redesign" do
+  it "supports block-less symbol doubles" do
     expect(dbl).to_not be_nil
   end
 end
