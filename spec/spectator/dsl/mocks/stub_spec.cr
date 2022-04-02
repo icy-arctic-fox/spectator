f1ce914b73f9c778d6a5ca6b5c5b76613fdc8d21
Michael Miller <icy.arctic.fox@gmail.com>
2022-04-02 12:07:30 -0600

    Better stub DSL tests

--- spec/spectator/dsl/mocks/stub_spec.cr
+++ spec/spectator/dsl/mocks/stub_spec.cr
@@ -1,11 +1,30 @@
 require "../../../spec_helper"
 
 Spectator.describe "Stub DSL" do
-  double(:foobar, foo: 42, bar: "baz")
+  double(:foobar, foo: 42, bar: "baz") do
+    stub abstract def other : String
+    stub abstract def null : Nil
+  end
+
   let(dbl) { double(:foobar) }
 
-  specify do
+  it "overrides default stubs" do
     allow(dbl).to receive(:foo).and_return(123)
     expect(dbl.foo).to eq(123)
+  end
+
+  it "overrides abstract stubs" do
+    allow(dbl).to receive(:other).and_return("test")
+    expect(dbl.other).to eq("test")
+  end
+
+  it "returns nil by default" do
+    allow(dbl).to receive(:null)
+    expect(dbl.null).to be_nil
+  end
+
+  it "raises on cast errors" do
+    allow(dbl).to receive(:foo).and_return(:xyz)
+    expect { dbl.foo }.to raise_error(TypeCastError, /Int32/)
   end
 end
