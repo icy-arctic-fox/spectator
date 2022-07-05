fb5f44ef1040e34b599c7b9529dc6da467096d40
Michael Miller <icy.arctic.fox@gmail.com>
2022-07-04 20:06:26 -0600

    Add 'new' and 'allocate' to reserved keywords

--- src/spectator/dsl/reserved.cr
+++ src/spectator/dsl/reserved.cr
@@ -2,6 +2,6 @@ module Spectator
   module DSL
     # Keywords that cannot be used in specs using the DSL.
     # These are either problematic or reserved for internal use.
-    RESERVED_KEYWORDS = %i[initialize finalize class]
+    RESERVED_KEYWORDS = %i[initialize finalize class new allocate]
   end
 end
