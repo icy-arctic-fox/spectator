da3fbc960765aae361214e44f02fc53d6107339b
Michael Miller <icy.arctic.fox@gmail.com>
2022-07-13 11:17:10 -0600

    Add spec for GitHub issue 43
    https://github.com/icy-arctic-fox/spectator/issues/43

---
+++ spec/issues/github_issue_43_spec.cr
@@ -0,0 +1,51 @@
+require "../spec_helper"
+
+class Person
+  def initialize(@dog = Dog.new)
+  end
+
+  def pet
+    @dog.pet
+  end
+
+  def pet_more
+    @dog.pet(5)
+  end
+end
+
+class Dog
+  def initialize
+  end
+
+  def pet(times = 2)
+    "woof" * times
+  end
+end
+
+Spectator.describe Person do
+  mock Dog
+
+  describe "#pet" do
+    it "pets the persons dog" do
+      dog = mock(Dog)
+      person = Person.new(dog)
+      allow(dog).to receive(pet()).and_return("woof")
+
+      result = person.pet
+
+      expect(dog).to have_received(pet()).with(2)
+    end
+  end
+
+  describe "#pet_more" do
+    it "pets the persons dog alot" do
+      dog = mock(Dog)
+      person = Person.new(dog)
+      allow(dog).to receive(pet()).and_return("woof")
+
+      result = person.pet_more
+
+      expect(dog).to have_received(pet()).with(5)
+    end
+  end
+end
