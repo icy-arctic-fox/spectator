icy-arctic-fox/spectator

commit aa9ca7a98e554cc464756037c2a809a61274a281

Don't return nil for methods using NoReturn

src/spectator/mocks/stubbable.cr
@@ -149,7 +149,7 @@ module Spectator
         if %stub = _spectator_find_stub(%call)
           # Cast the stub or return value to the expected type.
           # This is necessary to match the expected return type of the original method.
-          _spectator_cast_stub_value(%stub, %call, typeof({{original}}), {{method.return_type && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
+          _spectator_cast_stub_value(%stub, %call, typeof({{original}}), {{method.return_type && method.return_type.resolve != NoReturn && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
         else
           # Delegate missing stub behavior to concrete type.
           _spectator_stub_fallback(%call, typeof({{original}})) do
@@ -241,7 +241,7 @@ module Spectator
           # This is necessary to match the expected return type of the original method.
           {% if method.return_type %}
             # Return type restriction takes priority since it can be a superset of the original implementation.
-            _spectator_cast_stub_value(%stub, %call, {{method.return_type}}, {{method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
+            _spectator_cast_stub_value(%stub, %call, {{method.return_type}}, {{method.return_type.resolve != NoReturn && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
           {% elsif !method.abstract? %}
             # The method isn't abstract, infer the type it returns without calling it.
             _spectator_cast_stub_value(%stub, %call, typeof({{original}}))

spec/spectator/mocks/double_spec.cr
@@ -114,6 +114,27 @@ Spectator.describe Spectator::Double do
     end
   end
 
+  context "with a method that uses NoReturn" do
+    Spectator::Double.define(NoReturnDouble) do
+      abstract_stub abstract def oops : NoReturn
+    end
+
+    subject(dbl) { NoReturnDouble.new }
+
+    it "raises a TypeCastError when using a value-based stub" do
+      stub = Spectator::ValueStub.new(:oops, nil).as(Spectator::Stub)
+      dbl._spectator_define_stub(stub)
+      expect { dbl.oops }.to raise_error(TypeCastError, /NoReturn/)
+    end
+
+    it "raises when using an exception stub" do
+      exception = ArgumentError.new("bogus")
+      stub = Spectator::ExceptionStub.new(:oops, exception).as(Spectator::Stub)
+      dbl._spectator_define_stub(stub)
+      expect { dbl.oops }.to raise_error(ArgumentError, "bogus")
+    end
+  end
+
   context "with common object methods" do
     subject(dbl) do
       EmptyDouble.new([

spec/spectator/mocks/mock_spec.cr
@@ -400,6 +400,31 @@ Spectator.describe Spectator::Mock do
         end
       end
     end
+
+    context "with a method that uses NoReturn" do
+      abstract class Thing
+        abstract def oops : NoReturn
+      end
+
+      Spectator::Mock.define_subtype(:class, Thing, MockThing)
+
+      let(mock) { MockThing.new }
+
+      after_each { mock._spectator_clear_stubs }
+
+      it "raises a TypeCastError when using a value-based stub" do
+        stub = Spectator::ValueStub.new(:oops, nil).as(Spectator::Stub)
+        mock._spectator_define_stub(stub)
+        expect { mock.oops }.to raise_error(TypeCastError, /NoReturn/)
+      end
+
+      it "raises when using an exception stub" do
+        exception = ArgumentError.new("bogus")
+        stub = Spectator::ExceptionStub.new(:oops, exception).as(Spectator::Stub)
+        mock._spectator_define_stub(stub)
+        expect { mock.oops }.to raise_error(ArgumentError, "bogus")
+      end
+    end
   end
 
   describe "#inject" do
@@ -717,6 +742,33 @@ Spectator.describe Spectator::Mock do
           call = mock._spectator_calls.first
           expect(call.arguments).to eq(args)
         end
+      end
+    end
+
+    context "with a method that uses NoReturn" do
+      struct ::NoReturnThing
+        def oops : NoReturn
+          raise "oops"
+        end
+      end
+
+      Spectator::Mock.inject(:struct, ::NoReturnThing)
+
+      let(mock) { NoReturnThing.new }
+
+      after_each { mock._spectator_clear_stubs }
+
+      it "raises a TypeCastError when using a value-based stub" do
+        stub = Spectator::ValueStub.new(:oops, nil).as(Spectator::Stub)
+        mock._spectator_define_stub(stub)
+        expect { mock.oops }.to raise_error(TypeCastError, /NoReturn/)
+      end
+
+      it "raises when using an exception stub" do
+        exception = ArgumentError.new("bogus")
+        stub = Spectator::ExceptionStub.new(:oops, exception).as(Spectator::Stub)
+        mock._spectator_define_stub(stub)
+        expect { mock.oops }.to raise_error(ArgumentError, "bogus")
       end
     end
   end

spec/spectator/mocks/null_double_spec.cr
@@ -90,6 +90,27 @@ Spectator.describe Spectator::NullDouble do
     end
   end
 
+  context "with a method that uses NoReturn" do
+    Spectator::NullDouble.define(NoReturnDouble) do
+      abstract_stub abstract def oops : NoReturn
+    end
+
+    subject(dbl) { NoReturnDouble.new }
+
+    it "raises a TypeCastError when using a value-based stub" do
+      stub = Spectator::ValueStub.new(:oops, nil).as(Spectator::Stub)
+      dbl._spectator_define_stub(stub)
+      expect { dbl.oops }.to raise_error(TypeCastError, /NoReturn/)
+    end
+
+    it "raises when using an exception stub" do
+      exception = ArgumentError.new("bogus")
+      stub = Spectator::ExceptionStub.new(:oops, exception).as(Spectator::Stub)
+      dbl._spectator_define_stub(stub)
+      expect { dbl.oops }.to raise_error(ArgumentError, "bogus")
+    end
+  end
+
   context "with common object methods" do
     subject(dbl) do
       EmptyDouble.new([

commit 9efb6c95dd8d954b310e6ad3530cb25c1e798630

Workaround compiler bug introduced by aa9ca7a
BUG: trying to upcast Nil (Crystal::NilType) <- NoReturn (Crystal::NoReturnType) (Exception)
  from /crystal/src/compiler/crystal/codegen/cast.cr:668:5 in 'upcast_distinct'
  from /crystal/src/compiler/crystal/codegen/cast.cr:523:15 in 'upcast'
  from /crystal/src/compiler/crystal/codegen/codegen.cr:1369:11 in 'accept'
  from /crystal/src/compiler/crystal/codegen/codegen.cr:2274:9 in 'visit'
  from /crystal/src/compiler/crystal/syntax/visitor.cr:27:12 in 'accept'
...

src/spectator/mocks/stubbable.cr
@@ -149,7 +149,14 @@ module Spectator
         if %stub = _spectator_find_stub(%call)
           # Cast the stub or return value to the expected type.
           # This is necessary to match the expected return type of the original method.
-          _spectator_cast_stub_value(%stub, %call, typeof({{original}}), {{method.return_type && method.return_type.resolve != NoReturn && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
+          _spectator_cast_stub_value(%stub, %call, typeof({{original}}),
+          {{ if method.return_type && method.return_type.resolve == NoReturn
+               :no_return
+             elsif method.return_type && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)
+               :nil
+             else
+               :raise
+             end }})
         else
           # Delegate missing stub behavior to concrete type.
           _spectator_stub_fallback(%call, typeof({{original}})) do
@@ -241,7 +248,14 @@ module Spectator
           # This is necessary to match the expected return type of the original method.
           {% if method.return_type %}
             # Return type restriction takes priority since it can be a superset of the original implementation.
-            _spectator_cast_stub_value(%stub, %call, {{method.return_type}}, {{method.return_type.resolve != NoReturn && method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)}})
+            _spectator_cast_stub_value(%stub, %call, {{method.return_type}},
+              {{ if method.return_type.resolve == NoReturn
+                   :no_return
+                 elsif method.return_type.resolve <= Nil || method.return_type.is_a?(Union) && method.return_type.types.map(&.resolve).includes?(Nil)
+                   :nil
+                 else
+                   :raise
+                 end }})
           {% elsif !method.abstract? %}
             # The method isn't abstract, infer the type it returns without calling it.
             _spectator_cast_stub_value(%stub, %call, typeof({{original}}))
@@ -376,8 +390,11 @@ module Spectator
     # *stub* is the variable holding the stub.
     # *call* is the variable holding the captured method call.
     # *type* is the expected type to cast the value to.
-    # *nullable* indicates whether *type* can be nil or not.
-    private macro _spectator_cast_stub_value(stub, call, type, nullable = true)
+    # *fail_cast* indicates the behavior used when the value returned by the stub can't be cast to *type*.
+    # - `:nil` - return nil.
+    # - `:raise` - raise a `TypeCastError`.
+    # - `:no_return` - raise as no value should be returned.
+    private macro _spectator_cast_stub_value(stub, call, type, fail_cast = :nil)
       # Attempt to cast the stub to the method's return type.
       # If successful, return the value of the stub.
       # This is a common usage where the return type is simple and matches the stub type exactly.
@@ -390,29 +407,36 @@ module Spectator
         # Assert that it will (this should never fail).
         raise TypeCastError.new("Stub has no value") unless {{stub}}.responds_to?(:call)
 
-        # Get the value as-is from the stub.
-        # This will be compiled as a union of all known stubbed value types.
-        %value = {{stub}}.call({{call}})
+        {% if fail_cast == :no_return %}
+          {{stub}}.call({{call}})
+          raise TypeCastError.new("#{_spectator_stubbed_name} received message #{ {{call}} } and is attempting to return a value, but it shouldn't have returned (`NoReturn`).")
+        {% else %}
+          # Get the value as-is from the stub.
+          # This will be compiled as a union of all known stubbed value types.
+          %value = {{stub}}.call({{call}})
 
-        # Attempt to cast the value to the method's return type.
-        # If successful, which it will be in most cases, return it.
-        # The caller will receive a properly typed value without unions or other side-effects.
-        if %cast = %value.as?({{type}})
-          %cast
-        else
-          {% if nullable %}
-            nil
-          {% else %}
-            # The stubbed value was something else entirely and cannot be cast to the return type.
-            # There's something weird going on (compiler bug?) that sometimes causes this class lookup to fail.
-            %type = begin
-              %value.class.to_s
-            rescue
-              "<Unknown>"
-            end
-            raise TypeCastError.new("#{_spectator_stubbed_name} received message #{ {{call}} } and is attempting to return a `#{%type}`, but returned type must be `#{ {{type}} }`.")
-          {% end %}
-        end
+          # Attempt to cast the value to the method's return type.
+          # If successful, which it will be in most cases, return it.
+          # The caller will receive a properly typed value without unions or other side-effects.
+          if %cast = %value.as?({{type}})
+            %cast
+          else
+            {% if fail_cast == :nil %}
+              nil
+            {% elsif fail_cast == :raise %}
+              # The stubbed value was something else entirely and cannot be cast to the return type.
+              # There's something weird going on (compiler bug?) that sometimes causes this class lookup to fail.
+              %type = begin
+                %value.class.to_s
+              rescue
+                "<Unknown>"
+              end
+              raise TypeCastError.new("#{_spectator_stubbed_name} received message #{ {{call}} } and is attempting to return a `#{%type}`, but returned type must be `#{ {{type}} }`.")
+            {% else %}
+              {% raise "fail_cast must be :nil, :raise, or :no_return, but got: #{fail_cast}" %}
+            {% end %}
+          end
+        {% end %}
       end
     end
   end