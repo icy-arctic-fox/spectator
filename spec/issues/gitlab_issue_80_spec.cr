47a62ece789d8fe88bb05ef950078756c68dc299
Michael Miller <icy.arctic.fox@gmail.com>
2022-12-08 17:14:09 -0700

    Add reduced test code for GitLab issue 80
    https://gitlab.com/arctic-fox/spectator/-/issues/80
    Note: This test only triggers a compiler bug when the file is compiled by itself.
    Compiling/running the entire spec suite *does not* cause the bug.

---
+++ spec/issues/gitlab_issue_80_spec.cr
@@ -0,0 +1,29 @@
+require "../spec_helper"
+
+# https://gitlab.com/arctic-fox/spectator/-/issues/80#
+
+class Item
+end
+
+class ItemUser
+  @item = Item.new
+
+  def item
+    @item
+  end
+end
+
+Spectator.describe "test1" do
+  it "without mock" do
+    item_user = ItemUser.new
+    item = item_user.item
+    item == item
+  end
+end
+
+Spectator.describe "test2" do
+  mock Item do
+  end
+  it "without mock" do
+  end
+end
