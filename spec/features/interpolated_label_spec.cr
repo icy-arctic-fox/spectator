30602663fe87399e8572a90ca7464fc174fb4d31
Michael Miller <icy.arctic.fox@gmail.com>
2022-12-20 20:12:58 -0700

    Add tests for interpolated labels
    The context label test intentionally fails.
    This functionality still needs to be implemented.

---
+++ spec/features/interpolated_label_spec.cr
@@ -0,0 +1,22 @@
+require "../spec_helper"
+
+Spectator.describe "Interpolated Label" do
+  let(foo) { "example" }
+  let(bar) { "context" }
+
+  it "interpolates #{foo} labels" do |example|
+    expect(example.name).to eq("interpolates example labels")
+  end
+
+  context "within a \#{bar}" do
+    let(foo) { "multiple" }
+
+    it "interpolates context labels" do |example|
+      expect(example.group.name).to eq("within a context")
+    end
+
+    it "interpolates #{foo} levels" do |example|
+      expect(example.name).to eq("interpolates multiple levels")
+    end
+  end
+end
