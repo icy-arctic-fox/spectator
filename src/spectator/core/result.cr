9a3b0e0c7d9d5250387029b08c950518bff1e055
Michael Miller <icy.arctic.fox@gmail.com>
2024-06-23 23:01:22 -0600

    Add skip and pass/fail predicate methods

--- src/spectator/core/result.cr
+++ src/spectator/core/result.cr
@@ -3,11 +3,16 @@ module Spectator::Core
     Pass
     Fail
     Error
+    Skip
   end
 
+  # Information detailing the outcome of running an example.
   struct Result
+    # The status of the example.
     getter status : Status
 
+    # The exception that caused the example to fail.
+    # This will be nil if the example passed or was skipped.
     getter! exception : Exception
 
     def initialize(@status : Status, @exception = nil)
@@ -20,6 +25,14 @@ module Spectator::Core
       rescue ex
         new(:error, ex)
       end
+    end
+
+    def pass?
+      @status.pass?
+    end
+
+    def fail?
+      !@status.pass? && !@status.skip?
     end
   end
 end
