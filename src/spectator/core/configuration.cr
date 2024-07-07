16bb37958dd742aea7de075dfb0ba6a4ddc72f56
Michael Miller <icy.arctic.fox@gmail.com>
2024-07-07 12:27:04 -0600

    Initial configuration code

---
+++ src/spectator/core/configuration.cr
@@ -0,0 +1,76 @@
+require "../reporters/dots_reporter"
+require "./hooks"
+require "./sandbox"
+
+module Spectator
+  module Core
+    class Configuration
+      include Hooks
+
+      enum Order
+        Defined
+        Random
+        RecentlyModified
+      end
+
+      DEFAULT_FAIL_FAST_EXAMPLES =  1
+      DEFAULT_SLOWEST_EXAMPLES   = 10
+
+      property reporters do
+        [Reporters::DotsReporter.new] of Reporters::Reporter
+      end
+
+      def add_reporter(reporter : Reporters::Reporter) : Nil
+        reporters << reporter
+      end
+
+      def reporter=(reporter : Reporters::Reporter)
+        reporters = [reporter] of Reporters::Reporter
+        reporter
+      end
+
+      property? dry_run = false
+
+      property fail_fast = 1
+
+      def fail_fast? : Bool
+        @fail_fast > 0
+      end
+
+      def fail_fast=(flag : Bool)
+        @fail_fast = flag ? DEFAULT_FAIL_FAST_EXAMPLES : 0
+      end
+
+      property fail_if_no_examples = true
+
+      property profile_examples = 0
+
+      def profile_examples? : Bool
+        @profile_examples > 0
+      end
+
+      def profile_examples=(flag : Bool)
+        @profile_examples = flag ? DEFAULT_SLOWEST_EXAMPLES : 0
+      end
+
+      property order = Order::Defined
+
+      property seed = 0_u64
+
+      property error_exit_code = 1
+    end
+
+    class Sandbox
+      getter configuration = Configuration.new
+    end
+  end
+
+  def self.configuration
+    sandbox.configuration
+  end
+
+  def self.configure(& : Core::Configuration ->) : Core::Configuration
+    yield configuration
+    configuration
+  end
+end
