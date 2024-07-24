2b4fc4f19f27b6864fe7c3128a7b327d3b5afc73
Michael Miller <icy.arctic.fox@gmail.com>
2024-07-24 17:50:03 -0600

    Test positive and negated cases

--- spec/spectator/matchers/built_in/be_a_matcher_spec.cr
+++ spec/spectator/matchers/built_in/be_a_matcher_spec.cr
@@ -44,75 +44,143 @@ Spectator.describe BeAMatcher do
     end
   end
 
-  describe "#negative_failure_message" do
+  describe "#negated_failure_message" do
     it "returns the negative failure message" do
       matcher = BeAMatcher(String).new
-      expect(matcher.negative_failure_message("foo")).to eq("Expected \"foo\" not to be a String")
+      expect(matcher.negated_failure_message("foo")).to eq("Expected \"foo\" not to be a String")
     end
   end
 
   context "DSL" do
     describe "be_a" do
-      it "matches if the value is an instance of the type" do
-        expect do
-          expect(42).to be_a(Int32)
-        end.to pass_check
+      context "with .to" do
+        it "matches if the value is an instance of the type" do
+          expect do
+            expect(42).to be_a(Int32)
+          end.to pass_check
+        end
+
+        it "does not match if the value is not an instance of the type" do
+          expect do
+            expect("foo").to be_a(Int32)
+          end.to fail_check("Expected \"foo\" to be a Int32")
+        end
+
+        it "does not match if the value is nil" do
+          expect do
+            expect(nil).to be_a(Int32)
+          end.to fail_check("Expected nil to be a Int32")
+        end
+
+        it "matches if the value is a subclass of the type" do
+          expect do
+            expect(Child.new).to be_a(Base)
+          end.to pass_check
+        end
+
+        it "does not match if the value is a parent of the type" do
+          expect do
+            expect(Base.new).to be_a(Child)
+          end.to fail_check(/^Expected .*?Base.*? to be a Child$/)
+        end
       end
 
-      it "does not match if the value is not an instance of the type" do
-        expect do
-          expect("foo").to be_a(Int32)
-        end.to fail_check("Expected \"foo\" to be a Int32")
-      end
+      context "with .not_to" do
+        it "does not match if the value is an instance of the type" do
+          expect do
+            expect(42).not_to be_a(Int32)
+          end.to fail_check("Expected 42 not to be a Int32")
+        end
 
-      it "does not match if the value is nil" do
-        expect do
-          expect(nil).to be_a(Int32)
-        end.to fail_check("Expected nil to be a Int32")
-      end
+        it "matches if the value is not an instance of the type" do
+          expect do
+            expect("foo").not_to be_a(Int32)
+          end.to pass_check
+        end
 
-      it "matches if the value is a subclass of the type" do
-        expect do
-          expect(Child.new).to be_a(Base)
-        end.to pass_check
-      end
+        it "matches if the value is nil" do
+          expect do
+            expect(nil).not_to be_a(Int32)
+          end.to pass_check
+        end
 
-      it "does not match if the value is a parent of the type" do
-        expect do
-          expect(Base.new).to be_a(Child)
-        end.to fail_check(/^Expected .*?Base.*? to be a Child$/)
+        it "does not match if the value is a subclass of the type" do
+          expect do
+            expect(Child.new).not_to be_a(Base)
+          end.to fail_check(/^Expected .*?Child.*? not to be a Base$/)
+        end
+
+        it "matches if the value is a parent of the type" do
+          expect do
+            expect(Base.new).not_to be_a(Child)
+          end.to pass_check
+        end
       end
     end
 
     describe "be_an" do
-      it "matches if the value is an instance of the type" do
-        expect do
-          expect(42).to be_an(Int32)
-        end.to pass_check
+      context "with .to" do
+        it "matches if the value is an instance of the type" do
+          expect do
+            expect(42).to be_an(Int32)
+          end.to pass_check
+        end
+
+        it "does not match if the value is not an instance of the type" do
+          expect do
+            expect("foo").to be_an(Int32)
+          end.to fail_check("Expected \"foo\" to be a Int32")
+        end
+
+        it "does not match if the value is nil" do
+          expect do
+            expect(nil).to be_an(Int32)
+          end.to fail_check("Expected nil to be a Int32")
+        end
+
+        it "matches if the value is a subclass of the type" do
+          expect do
+            expect(Child.new).to be_an(Base)
+          end.to pass_check
+        end
+
+        it "does not match if the value is a parent of the type" do
+          expect do
+            expect(Base.new).to be_an(Child)
+          end.to fail_check(/^Expected .*?Base.*? to be a Child$/)
+        end
       end
 
-      it "does not match if the value is not an instance of the type" do
-        expect do
-          expect("foo").to be_an(Int32)
-        end.to fail_check("Expected \"foo\" to be a Int32")
-      end
+      context "with .not_to" do
+        it "does not match if the value is an instance of the type" do
+          expect do
+            expect(42).not_to be_an(Int32)
+          end.to fail_check("Expected 42 not to be a Int32")
+        end
 
-      it "does not match if the value is nil" do
-        expect do
-          expect(nil).to be_an(Int32)
-        end.to fail_check("Expected nil to be a Int32")
-      end
+        it "matches if the value is not an instance of the type" do
+          expect do
+            expect("foo").not_to be_an(Int32)
+          end.to pass_check
+        end
 
-      it "matches if the value is a subclass of the type" do
-        expect do
-          expect(Child.new).to be_an(Base)
-        end.to pass_check
-      end
+        it "matches if the value is nil" do
+          expect do
+            expect(nil).not_to be_an(Int32)
+          end.to pass_check
+        end
 
-      it "does not match if the value is a parent of the type" do
-        expect do
-          expect(Base.new).to be_an(Child)
-        end.to fail_check(/^Expected .*?Base.*? to be a Child$/)
+        it "does not match if the value is a subclass of the type" do
+          expect do
+            expect(Child.new).not_to be_an(Base)
+          end.to fail_check(/^Expected .*?Child.*? not to be a Base$/)
+        end
+
+        it "matches if the value is a parent of the type" do
+          expect do
+            expect(Base.new).not_to be_an(Child)
+          end.to pass_check
+        end
       end
     end
   end
