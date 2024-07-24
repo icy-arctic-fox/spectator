a64f9d4479048a68fb6d209c0e133f745f8ce063
Michael Miller <icy.arctic.fox@gmail.com>
2024-07-24 12:49:45 -0600

    Define custom matcher to check for passing results

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,11 +1,26 @@
 require "../src/spectator"
 
-Spectator::Matchers.define(:be_successful) do
-  match(block: true) do |args|
-    p! yield
+Spectator::Matchers.define :be_successful do
+  @result : Spectator::Core::Result?
+
+  match(block: true) do
+    result = Spectator::Core::Result.capture do
+      yield
+    end
+    @result = result
+    result.passed?
   end
 
   failure_message(block: true) do
-    "expected thing to be successful"
+    result = @result.not_nil!
+    <<-END_OF_MESSAGE
+      Expected block to be successful, but the result was #{result.status}
+
+      Error: #{result.error_message}
+    END_OF_MESSAGE
+  end
+
+  negated_failure_message(block: true) do
+    "Expected block not to be successful"
   end
 end
