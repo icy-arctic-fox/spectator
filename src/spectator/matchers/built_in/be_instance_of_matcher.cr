f83a2b1f3b1dbbe7acac5c99b92aa2bdd44b368a
Michael Miller <icy.arctic.fox@gmail.com>
2024-07-25 23:50:50 -0600

    Improve failure messages

--- src/spectator/matchers/built_in/be_instance_of_matcher.cr
+++ src/spectator/matchers/built_in/be_instance_of_matcher.cr
@@ -5,11 +5,30 @@ module Spectator::Matchers::BuiltIn
     end
 
     def failure_message(actual_value) : String
-      "Expected #{actual_value.pretty_inspect} to be an instance of #{T}"
+      if actual_value.is_a?(T)
+        <<-MESSAGE
+         Expected: #{actual_value.pretty_inspect}
+          to be a: #{T}
+        but was a: #{actual_value.class}
+        
+        #{actual_value.class} is a sub-type of #{T}.
+        Using `be_instance_of` ensures the type matches EXACTLY.
+        If you want to match sub-types, use `be_a` instead.
+        MESSAGE
+      else
+        <<-MESSAGE
+         Expected: #{actual_value.pretty_inspect}
+          to be a: #{T}
+        but was a: #{actual_value.class}
+        MESSAGE
+      end
     end
 
     def negated_failure_message(actual_value) : String
-      "Expected #{actual_value.pretty_inspect} not to be an instance of #{T}"
+      <<-MESSAGE
+         Expected: #{actual_value.pretty_inspect}
+      not to be a: #{T}
+      MESSAGE
     end
   end
 end
