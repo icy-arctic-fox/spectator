7609817c39f552c7d7b3655ab831bd0e460c6c4c
Michael Miller <icy.arctic.fox@gmail.com>
2024-07-27 23:03:32 -0600

    Whitespace

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -14,9 +14,9 @@ Spectator::Matchers.define :pass_check do
   failure_message(block: true) do
     result = @result.not_nil!("`match` must be called before `failure_message`")
     <<-END_OF_MESSAGE
-      Expected check to pass, but the result was #{result.status}
+    Expected check to pass, but the result was #{result.status}
 
-      Error: #{result.error_message}
+    Error: #{result.error_message}
     END_OF_MESSAGE
   end
 end
@@ -37,10 +37,10 @@ Spectator::Matchers.define :fail_check, message : String | Regex do
     result = @result.not_nil!("`match` must be called before `failure_message`")
     if result.failed?
       <<-END_OF_MESSAGE
-        The check failed but produced an unexpected error message.
+      The check failed but produced an unexpected error message.
 
-        Expected: #{@message.pretty_inspect}
-             got: #{result.error_message.pretty_inspect}
+      Expected: #{@message.pretty_inspect}
+           got: #{result.error_message.pretty_inspect}
       END_OF_MESSAGE
     else
       "Expected check to fail, but the result was #{result.status}"
