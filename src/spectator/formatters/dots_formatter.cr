d9a24332816ff63c2a0f33e22ebe5d9b9867e508
Michael Miller <icy.arctic.fox@gmail.com>
2024-08-04 22:12:59 -0600

    Remove constants

--- src/spectator/formatters/dots_formatter.cr
+++ src/spectator/formatters/dots_formatter.cr
@@ -5,25 +5,15 @@ module Spectator::Formatters
   class DotsFormatter < Formatter
     include CommonTextOutput
 
-    PASS_CHAR  = '.'
-    FAIL_CHAR  = 'F'
-    ERROR_CHAR = 'E'
-    SKIP_CHAR  = '*'
-
-    PASS_STYLE  = Style::Success
-    FAIL_STYLE  = Style::Error
-    ERROR_STYLE = Style::Error
-    SKIP_STYLE  = Style::Warning
-
     def example_started(example : Core::Example) : Nil
     end
 
     def example_finished(result : Core::ExecutionResult) : Nil
       case result.status
-      in .pass?  then printer.print PASS_CHAR, style: PASS_STYLE
-      in .fail?  then printer.print FAIL_CHAR, style: FAIL_STYLE
-      in .error? then printer.print ERROR_CHAR, style: ERROR_STYLE
-      in .skip?  then printer.print SKIP_CHAR, style: SKIP_STYLE
+      in .pass?  then printer.print('.', style: :success)
+      in .fail?  then printer.print('F', style: :error)
+      in .error? then printer.print('E', style: :error)
+      in .skip?  then printer.print('*', style: :warning)
       end
     end
 
