d234c1946ad1a08a6a424aa0dde193eb092f1d64
Michael Miller <icy.arctic.fox@gmail.com>
2024-11-29 22:44:11 -0700

    Fix formatting of failure messages with `match` matcher

--- src/spectator/matchers/built_in/match_matcher.cr
+++ src/spectator/matchers/built_in/match_matcher.cr
@@ -20,7 +20,7 @@ module Spectator::Matchers::BuiltIn
       printer << "to match: " << description_of(@expected_value)
     end
 
-    def negated_failure_message(printer : Formatters::Printer, actual_value) : Nil
+    def print_negated_failure_message(printer : Formatters::Printer, actual_value) : Nil
       printer << "    Expected: " << description_of(actual_value) << EOL
       printer << "not to match: " << description_of(@expected_value)
     end
