bd41f42ede41662c58c706d7744d68d1b7be8b3b
Michael Miller <icy.arctic.fox@gmail.com>
2024-11-29 20:46:58 -0700

    Work around segfault by making private class names unique
    A segfault would occur when the private classes in `be_instance_of_matcher_spec.cr` and `be_a_matcher_spec.cr` had the same names.
    The segfault occurred in Class' `name` method when attempting to print it.
    
    Stacktrace:
    
    Invalid memory access (signal 11) at address 0x0
    [0x5c46e9e58906] *Exception::CallStack::print_backtrace:Nil +118 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x5c46e9e24e26] ~procProc(Int32, Pointer(LibC::SiginfoT), Pointer(Void), Nil) +310 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x7e1f2cc42520] ?? +138672360138016 in /lib/x86_64-linux-gnu/libc.so.6
    [0x5c46e9e84613] *Slice(T)::new:read_only<Pointer(UInt8), Int32, Bool>:Slice(UInt8) +35 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x5c46e9e59715] *String#to_slice:Slice(UInt8) +37 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x5c46e9e5b928] *String#to_s<String::Builder>:Nil +24 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x5c46e9e7f00b] *String::Builder +27 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x5c46e9fdc017] *Base+ +23 in /home/michael/.cache/crystal/crystal-run-spec.tmp
    [0x7e1f2ce610c0] ???

--- spec/spectator/matchers/built_in/be_instance_of_matcher_spec.cr
+++ spec/spectator/matchers/built_in/be_instance_of_matcher_spec.cr
@@ -1,9 +1,14 @@
 require "../../../spec_helper"
 
-private class Base
+# NOTE: The names of these classes must be different from those in `be_a_matcher_spec.cr`.
+#       Otherwise, a segment fault will occur if both specs are run in the same process.
+#       This appears to be a bug in Crystal, but I've been unable to provide a minimal reproducible example.
+#       To reproduce, remove `Inst` from the class names and run `crystal spec`.
+
+private class InstBase
 end
 
-private class Derived < Base
+private class InstDerived < InstBase
 end
 
 alias BeInstanceOfMatcher = Spectator::Matchers::BuiltIn::BeInstanceOfMatcher
@@ -11,18 +16,18 @@ alias BeInstanceOfMatcher = Spectator::Matchers::BuiltIn::BeInstanceOfMatcher
 Spectator.describe BeInstanceOfMatcher do
   describe "#matches?" do
     it "returns true if the object is the exact type" do
-      matcher = BeInstanceOfMatcher(Base).new
-      expect(matcher.matches?(Base.new)).to be_true
+      matcher = BeInstanceOfMatcher(InstBase).new
+      expect(matcher.matches?(InstBase.new)).to be_true
     end
 
     it "returns false if the object is a sub-type" do
-      matcher = BeInstanceOfMatcher(Base).new
-      expect(matcher.matches?(Derived.new)).to be_false
+      matcher = BeInstanceOfMatcher(InstBase).new
+      expect(matcher.matches?(InstDerived.new)).to be_false
     end
 
     it "returns false if the object is a parent type" do
-      matcher = BeInstanceOfMatcher(Derived).new
-      expect(matcher.matches?(Base.new)).to be_false
+      matcher = BeInstanceOfMatcher(InstDerived).new
+      expect(matcher.matches?(InstBase.new)).to be_false
     end
 
     it "returns false if the object is an unrelated type" do
@@ -42,14 +47,14 @@ Spectator.describe BeInstanceOfMatcher do
     end
 
     it "returns an extended failure message for sub-types" do
-      matcher = BeInstanceOfMatcher(Base).new
-      object = Derived.new
+      matcher = BeInstanceOfMatcher(InstBase).new
+      object = InstDerived.new
       expect(matcher.failure_message(object)).to eq <<-MESSAGE
          Expected: #{object.pretty_inspect}
           to be a: Base
-        but was a: Derived
+        but was a: InstDerived
 
-        Derived is a sub-type of Base.
+        InstDerived is a sub-type of Base.
         Using `be_instance_of` ensures the type matches EXACTLY.
         If you want to match sub-types, use `be_a` instead.
         MESSAGE
@@ -76,31 +81,27 @@ Spectator.describe BeInstanceOfMatcher do
         end
 
         it "does not match if the object is a sub-type" do
-          object = Derived.new
+          object = InstDerived.new
           expect do
-            expect(object).to be_instance_of(Base)
+            expect(object).to be_instance_of(InstBase)
           end.to fail_check <<-MESSAGE
              Expected: #{object.pretty_inspect}
               to be a: Base
-            but was a: Derived
+            but was a: InstDerived
 
-            Derived is a sub-type of Base.
+            InstDerived is a sub-type of Base.
             Using `be_instance_of` ensures the type matches EXACTLY.
             If you want to match sub-types, use `be_a` instead.
             MESSAGE
         end
 
         it "does not match if the object is a parent type" do
-          # Enabling this test causes a segfault.
-          # This only occurs when running all specs, not just this file.
-          # To reproduce, remove the following `skip` line and run `crystal spec`.
-          skip "This test causes a segfault for an unknown and unrelated reason."
-          object = Base.new
+          object = InstBase.new
           expect do
-            expect(object).to be_instance_of(Derived)
+            expect(object).to be_instance_of(InstDerived)
           end.to fail_check <<-MESSAGE
              Expected: #{object.pretty_inspect}
-              to be a: Derived
+              to be a: InstDerived
             but was a: Base
             MESSAGE
         end
@@ -128,17 +129,13 @@ Spectator.describe BeInstanceOfMatcher do
 
         it "matches if the object is a sub-type" do
           expect do
-            expect(Derived.new).not_to be_instance_of(Base)
+            expect(InstDerived.new).not_to be_instance_of(InstBase)
           end.to pass_check
         end
 
         it "matches if the object is a parent type" do
-          # Enabling this test causes a segfault.
-          # This only occurs when running all specs, not just this file.
-          # To reproduce, remove the following `skip` line and run `crystal spec`.
-          skip "This test causes a segfault for an unknown and unrelated reason."
           expect do
-            expect(Base.new).not_to be_instance_of(Derived)
+            expect(InstBase.new).not_to be_instance_of(InstDerived)
           end.to pass_check
         end
 
