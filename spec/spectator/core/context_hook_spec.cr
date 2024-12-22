aa28d8f35c510c86f3d4d4c8dc59ddcce78fa3ed
Michael Miller <icy.arctic.fox@gmail.com>
2024-12-22 16:40:26 -0700

    Add a test case for ContextHook

--- spec/spectator/core/context_hook_spec.cr
+++ spec/spectator/core/context_hook_spec.cr
@@ -3,6 +3,16 @@ require "../../spec_helper"
 alias ContextHook = Spectator::Core::ContextHook
 
 Spectator.describe ContextHook do
+  it "stores the attributes" do
+    location = Spectator::Core::LocationRange.new("foo.cr", 42)
+    hook = ContextHook.new(:before, location) { }
+    expect(hook).to have_attributes(
+      position: ContextHook::Position::Before,
+      location: location,
+      exception: nil,
+    )
+  end
+
   describe "#call" do
     it "calls the block" do
       called = false
